12cbac8a085ad786276dc9c4d274d42a20aa0d13
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2023-12-22 12:32:23 +0100

    Feature: Initial setup
    List of changes:
    - initialized empty flutter project
    - added core libraries to pubspec.yaml (auto_route, equatable, flutter_bloc, get_it, intl, logger, build_runner)
    - updated .gitignore

---
+++ android/app/src/main/kotlin/it/candylabs/breeder/breeder/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.candylabs.breeder.breeder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/router/router.gr.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i12;
+
+abstract class $AppRouter extends _i12.RootStackRouter {
+  $AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, _i12.PageFactory> pagesMap = {};
+}
